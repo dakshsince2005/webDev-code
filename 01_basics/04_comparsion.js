da9ee43f4e177257e4d348523503265c0930e899
dakshsince2005 <92094076+dakshsince2005@users.noreply.github.com>
2024-03-28 17:51:12 +0000

    #

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,7 @@
+// console.log("2" > 1);
+// console.log("22" > 13232);
+
+// comparisons convert null into 0 thus giving the respected output
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
