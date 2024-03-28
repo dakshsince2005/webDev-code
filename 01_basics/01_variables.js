6c2ff3777d489e086f6e705a7ca575f70f0b8ffe
dakshsince2005 <92094076+dakshsince2005@users.noreply.github.com>
2024-03-28 03:03:16 +0000

    variables learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456
+let accountEmail = "daksh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountCity = "Bhopal"
+// console.log(accountId);
+// console.log(accountCity);
+console.table([accountEmail, accountId, accountPassword, accountCity])
