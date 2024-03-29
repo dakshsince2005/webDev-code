048f76015519c7f36a348a3695ebc7759c42c454
dakshsince2005 <92094076+dakshsince2005@users.noreply.github.com>
2024-03-29 13:05:28 +0000

    #

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+let str1 = "Daksh"
+let str2 = " Saini"
+// let con = str1.concat(str2)
+// console.log(con);
+
+// BackTicks(`my name is ${var}`)
+// console.log(`My name is ${str1.concat(str2)}`)
+
+const gameName = new String('Daksh');
+console.log(gameName.indexOf('a')); //This function tells the index of the element you entered
+console.log(gameName[3]); //To access the element on the respective index you entered 
+console.log(gameName.length); //To print the length of the string and it's a method not a function
+console.log(gameName.toUpperCase()); //Prints all elements in upper case
+console.log(gameName.toLowerCase()); //Prints all elements in lower case
