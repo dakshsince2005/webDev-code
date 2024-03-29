e39c0465f441b60115d0974e6f4b3121b5f7bee2
dakshsince2005 <92094076+dakshsince2005@users.noreply.github.com>
2024-03-29 16:22:11 +0000

    #

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -12,3 +12,19 @@ console.log(gameName[3]); //To access the element on the respective index you en
 console.log(gameName.length); //To print the length of the string and it's a method not a function
 console.log(gameName.toUpperCase()); //Prints all elements in upper case
 console.log(gameName.toLowerCase()); //Prints all elements in lower case
+console.log(gameName.charAt(3)); //Prints the character of the index number you've entered
+console.log(gameName.substring(0, 3)); //Prints the string from the index number you entered first till the end index-1 (start till end-1)
+console.log(gameName.slice(-5, 2)); //similar to slicing but we can pass negetive index which will print from the end 
+// .trim() function removes all the empty spaces from the sides of the string
+// you can use trimstart and trimend functions as well to remove spaces from a paricular side 
+
+const url = "https://google.com/daksh%20saini"
+console.log(url.replace("%20", "-")); //replace function repalces/changes a certain character or element from the string 
+console.log(url.includes('daksh')); //tells weather the value entered exists in the string or not (if yes returns ture else false)
+
+const words = "daksh-saini-from-jaipur"
+console.log(words.split("-")); //splits the string of the basis of the seperator in this case which is "-"
+
+// Points to be noted
+// Use back ticks instead of "+" (console.log(`my name is ${var1}`)) like this
+// And there are more string functions and methods you can explore them on mdn documentations
