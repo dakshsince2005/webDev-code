4d0f3bada5073b154177dd4795481c9236b936b9
dakshsince2005 <92094076+dakshsince2005@users.noreply.github.com>
2024-04-15 13:05:51 +0000

    #

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,9 +1,14 @@
-// Why we should not use var in the code:-
-var a = 200;
-let b = 300;
-const c = 400;
-if(true) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+/*
+Why we should not use var in the code:-
+So in the the code below the variables cannot be accessed outside of the scope execpt var cuz the concept of scope does not apply on var
+if (true) {
+    var a = 200;
+    let b = 300;
+    const c = 400;
 }
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+
+// Any and everything outside curly bracets have GLOBLE SCOPE and inside have BLOCK SCOPE
